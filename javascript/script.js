b505de2bae8dcb3eb9bc9be53a93e230396c584a
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-19 11:23:08 +0100

    feat: modifiche per integrare overlay

--- javascript/script.js
+++ javascript/script.js
@@ -11,19 +11,26 @@ axios
   .catch((errore) => {
     console.log("errore:", errore);
   });
-/* funzione che lavora i dati ricevuti e stampa in pagina */
+
+/* funzione che lavora i dati ricevuti*/
 function processPictures(array) {
   const row = document.querySelector(".row");
   array.forEach((photo) => {
     const { date, title, url } = photo;
-    const col = document.createElement("div");
-    col.classList.add("col");
-    col.innerHTML = `<div class="card">
+    const colonna = creaColonna();
+    colonna.innerHTML = `<div class="card">
               <img class="pin" src="./img/pin.svg" alt="" />
               <img src="${url}" alt="${title}" />
               <p>${date}</p>
               <h3>${title.toUpperCase()}</h3>
            </div>`;
-    row.append(col);
+
+    row.append(colonna);
   });
+}
+
+function creaColonna() {
+  const col = document.createElement("div");
+  col.classList.add("col");
+  return col;
 }
